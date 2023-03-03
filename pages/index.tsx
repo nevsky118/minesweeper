a86e2e91c879ed20dada84d0441c64abb0bbdf71
nevsky118 <nevsky118@gmail.com>
2023-03-03 20:18:43 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,26 @@ import Board from '@/components/Board';
 import DifficultySelect from '@/components/DifficultySelect';
 import Header from '@/components/Header';
 import Stats from '@/components/Stats';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<main style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-			<DifficultySelect />
-			<div style={{ display: 'flex', gap: '8px' }}>
-				<div className="container">
-					<Header />
-					<Board />
-				</div>
+		<>
+			<Head>
+				<title>Minesweeper</title>
+			</Head>
 
-				<Stats />
-			</div>
-		</main>
+			<main style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+				<DifficultySelect />
+				<div style={{ display: 'flex', gap: '8px' }}>
+					<div className="container">
+						<Header />
+						<Board />
+					</div>
+
+					<Stats />
+				</div>
+			</main>
+		</>
 	);
 }
